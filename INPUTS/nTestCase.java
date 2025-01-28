c348586528dffb579a6b2e3127e1d3d0e2a1db52
YashGupta <yashgupta32a@gmail.com>
2025-01-28 10:23:03 +0530

    Update nTestCase.java

--- INPUTS/nTestCase.java
+++ INPUTS/nTestCase.java
@@ -1,7 +1,7 @@
 package INPUTS;
 import java.util.*;
 
-public class nTestCase {
+public class Main {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in) ;
